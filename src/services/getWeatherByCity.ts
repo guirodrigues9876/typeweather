614d6f061fd2e26f49d2a7524ead8197afa7c016
Guilherme Rodrigues <guilherme121225@gmail.com>
2023-09-01 18:10:43 -0300

    switching to ts

--- src/services/getWeatherByCity.ts
+++ src/services/getWeatherByCity.ts
@@ -4,8 +4,38 @@ import { api } from "./api";
 import { getNextDays } from "../utils/getNextDays";
 import { weatherIcons } from "../utils/weatherIcons";
 
-export async function getWeatherByCity({ latitude, longitude }) {
-  const { data } = await api.get(`/forecast?lat=${latitude}&lon=${longitude}`);
+interface GetWeatherByCityProps {
+  latitude: number;
+  longitude: number;
+}
+
+type WeatherIconKeysProps = 'Clouds' | 'Rain' | 'Clear' | 'Snow';
+
+export interface WeatherAPIResponseProps {
+  list: {
+    dt_txt: string;
+    pop: number;
+    main: {
+      temp: number;
+      temp_min: number;
+      temp_max: number;
+      feels_like: number;
+      humidity: number;
+      temp_kf: number;
+    },
+    wind: {
+      speed: number;
+    },
+    weather: {
+      main: WeatherIconKeysProps;
+      description: string;
+    }[];
+  }[];
+}
+
+export async function getWeatherByCity({ latitude, longitude }: GetWeatherByCityProps) {
+  const { data } = await api.get<WeatherAPIResponseProps>(`/forecast?lat=${latitude}&lon=${longitude}`);
+  
   const { main, weather, wind, pop } = data.list[0];
 
   console.log(weather)
